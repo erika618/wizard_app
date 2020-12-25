4a875cae775db9918b951f278acf005f39b036c6
erika618 <68714247+erika618@users.noreply.github.com>
2020-12-25 23:44:19 +0900

    Addressモデルの修正。
    endが一つ多かったため。

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,5 @@
 class Address < ApplicationRecord
   # 住所情報を入力した時点で外部キーがnilであってもDBに保存する
   belongs_to :user, optional: true
-  validates :postal_code, :address ,presence: true
-end
+  validates :postal_code, :address, presence: true
 end
